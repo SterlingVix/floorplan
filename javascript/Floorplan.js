5034a6b5b67aea4392a6a25632119afa8aae4459
AaronMelocik <amelocik@gmail.com>
2015-09-02 19:46:01 -0700

    Formatting update, add quick TODO message.

--- javascript/Floorplan.js
+++ javascript/Floorplan.js
@@ -289,6 +289,8 @@ Floorplan.prototype.createModal = function (boothData) {
     // Create popup modal element
     var modal = $('<div class="modal fade" id="modal-' + boothData.boothNumber + '" tabindex="-1" role="dialog"></div>');
 
+    // TODO: Update for "available" booths here!
+    
     // Long text string for innerHTML of modal element
     var modalHTML = '<div class="modal-dialog" role="document">' + '    <div class="modal-content">' + '        <div class="modal-header">' + '            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '            <h4 class="modal-title" id="myModalLabel">' + boothData.company + ' (Booth ' + boothData.boothNumber + ')</h4>' + '            <img class="company-logo" src="' + this.pathToLogos + boothData.logo + '" />' + '        </div>' + '        <div class="modal-body">' + '            <p class="company-personell">' + personellString + '</p>' + '            <p class="company-information">' + boothData.information + '</p>' + '        </div>' + '        <div class="modal-footer">' + '            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' + '            <!-- <button type="button" class="btn btn-primary">EXTRA BUTTON</button> -->' + '        </div>' + '    </div>' + '</div>';
 
